7340c2f6fd5f97bb7120b80a7b93f2a6d8662d3e
Vadimka Komissarov <admin@vkom.cc>
2022-10-03 15:09:37 +0000

    some code optimizing for deluge

--- deluge/deluge.go
+++ deluge/deluge.go
@@ -1,9 +1,7 @@
 package deluge
 
 import (
-	"encoding/json"
 	"net"
-	"os"
 
 	delugeclient "github.com/MindHunter86/go-libdeluge"
 	"github.com/rs/zerolog"
@@ -43,64 +41,4 @@ func NewClient(c *cli.Context, log *zerolog.Logger) (*Client, error) {
 	return &Client{
 		deluge: deluge,
 	}, e
-}
-
-func (m *Client) GetTorrentsStatus() (e error) {
-	var trrs map[string]*delugeclient.TorrentStatus
-	if trrs, e = m.deluge.TorrentsStatus(delugeclient.StateUnspecified, nil); e != nil {
-		return
-	}
-
-	je := json.NewEncoder(os.Stdout)
-	je.SetIndent("", "  ")
-
-	if e = je.Encode(trrs); e != nil {
-		return
-	}
-
-	return
-}
-
-func (m *Client) GetTorrents() (map[string]*delugeclient.TorrentStatus, error) {
-	return m.deluge.TorrentsStatus(delugeclient.StateUnspecified, nil)
-}
-
-func (m *Client) GetTorrentsHashes() ([]string, error) {
-	var e error
-	var trrs map[string]*delugeclient.TorrentStatus
-
-	if trrs, e = m.deluge.TorrentsStatus(delugeclient.StateUnspecified, nil); e != nil {
-		return nil, e
-	}
-
-	var hashes []string
-	for hash, _ := range trrs {
-		hashes = append(hashes, hash)
-	}
-
-	gLog.Debug().Int("hashes_length", len(hashes)).Msg("the torrnets hashes has been collected")
-	return hashes, e
-}
-
-func (m *Client) GetWeakTorrents() ([]*delugeclient.TorrentStatus, error) {
-	trrs, e := m.GetTorrents()
-	if e != nil {
-		return nil, e
-	}
-
-	var weakTrrs []*delugeclient.TorrentStatus
-
-	for hash, torrent := range trrs {
-		if torrent.SeedingTime < 86400 {
-			continue
-		}
-		if torrent.Ratio > 0.5 {
-			continue
-		}
-
-		gLog.Info().Str("hash", hash).Str("torrnet_name", torrent.Name).Msg("marking as weak ...")
-		weakTrrs = append(weakTrrs, torrent)
-	}
-
-	return weakTrrs, e
 }
