1087b61d00097ee4eff7aee356fd382bc92ba34f
Vadimka Komissarov <admin@vkom.cc>
2022-10-22 09:17:02 +0000

    remove debug subcmd

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/MindHunter86/aniliSeeder/anilibria"
 	application "github.com/MindHunter86/aniliSeeder/app"
 	appcli "github.com/MindHunter86/aniliSeeder/cli"
 	"github.com/rs/zerolog"
@@ -307,19 +306,6 @@ func main() {
 			Usage: "",
 			Action: func(c *cli.Context) error {
 				return appcli.TestDial(c, "")
-			},
-		},
-		&cli.Command{
-			Name:  "sessions",
-			Usage: "",
-			Action: func(c *cli.Context) (e error) {
-				var gAniApi *anilibria.ApiClient
-				if gAniApi, e = anilibria.NewApiClient(c, &log); e != nil {
-					return
-				}
-
-				_, e = gAniApi.GetActiveSessions()
-				return e
 			},
 		},
 	}
